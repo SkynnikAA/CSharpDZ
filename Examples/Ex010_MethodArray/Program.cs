be70b65472f2ec5ab9da3cd3ba266a2f8789e94b
Skrynnik Aleksander <Devid99@yandex.ru>
2022-10-18 19:13:21 +0500

    сохранил Examples/Ex010_MethodArray/

---
+++ Examples/Ex010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arrey = { 1, 12, 31, 4, 15, 16, 4, 17, 18 };
+int n = arrey.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (arrey[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
